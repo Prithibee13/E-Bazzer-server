c88047812fbf0632ac217554d6e9d3ec57f3d92a
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-08-09 23:26:51 +0600

    S-l

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ async function run ()
         })
 
 
-         app.get('/products/:id' , async (req,res)=>
+         /* app.get('/products/:id' , async (req,res)=>
         {
             const id = req.params.id;
             const query={"_id": ObjectId(id)};
@@ -164,7 +164,7 @@ async function run ()
                 
             console.log(item);
             res.send(item)
-        })
+        }) */
          
 
         
